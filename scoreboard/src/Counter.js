81a679e7939723b0b243718123d963eaf5b3f31c
DelfinMong <delfdevs@gmail.com>
2021-10-26 19:08:20 -0400

    incdec

--- scoreboard/src/Counter.js
+++ scoreboard/src/Counter.js
@@ -8,15 +8,34 @@ class Counter extends React.Component {
             score : 0
         }
     }
+
+    incrementScore =() => {
+        this.setState({
+           score: this.state.score +1
+        })
+    }
+
+    decrementScore = () => {
+        this.setState({
+            score : this.state.score -1
+        })
+    }
+
     render() {
     return (
         <div className="counter">
-          <buttom className="counter-action decrement"> - </buttom>
+          <buttom className="counter-action decrement" onClick={this.decrementScore}> - </buttom>
           <buttom className="counter-score"> {this.state.score} </buttom>
-          <buttom className="counter-action increment"> + </buttom>
+          <buttom className="counter-action increment" onClick={this.incrementScore}> + </buttom>
         </div>
     )
   } 
 }
 
 export default Counter
+
+// { binding }
+// onClick={this.incrementScore.bind(this)}
+// onClick={() => this.incrementScore()}
+
+// use arrow function handling events ( need to bind this)
