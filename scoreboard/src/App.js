18cec6192098c9b77bf77b04fb89e2193c191afc
DelfinMong <delfdevs@gmail.com>
2021-10-27 01:01:13 -0400

    stats

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
       <div className="scoreboard">
        <Header
           title="Scoreboard" 
-          totalPlayer={this.state.players.length}
+          players={this.state.players}
         />
   
         {/* Players list */}
