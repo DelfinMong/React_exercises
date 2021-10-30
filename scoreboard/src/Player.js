b1195f059c3fd043d2c7a8c03dd18848db4e50ea
DelfinMong <delfdevs@gmail.com>
2021-10-30 00:56:13 -0400

    ref

--- scoreboard/src/Player.js
+++ scoreboard/src/Player.js
@@ -4,21 +4,30 @@ import Counter from "./Counter"
 
 class Player extends PureComponent  {
     render() {
-    console.log(this.props.name +  "rendered");
-    return (
-      <div className="player">
-        <span className="player-name"> 
-          <button className="remove-player" 
-               onClick={() => this.props.removePlayer(this.props.id)}> X </button>
-          {this.props.name} 
-        </span>
-        <Counter
-         score={this.props.score}
-         changeScore={this.props.changeScore}
-         index={this.props.index}
-         />
-      </div>
-    )
+      
+      const {
+        name,
+        id,
+        score,
+        index,
+        removePlayer,
+        changeScore
+      } = this.props;
+
+      return (
+        <div className="player">
+          <span className="player-name"> 
+            <button className="remove-player" 
+                onClick={() => removePlayer(id)}> X </button>
+            {name} 
+          </span>
+          <Counter
+          score={score}
+          changeScore={changeScore}
+          index={index}
+          />
+        </div>
+      )
     }
   }
 
