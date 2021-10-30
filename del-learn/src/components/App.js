6327077fe6858cebbb2e07eb0d4fcbaa53c34e8b
DelfinMong <delfdevs@gmail.com>
2021-10-30 19:20:44 -0400

    browserrouter

--- del-learn/src/components/App.js
+++ del-learn/src/components/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../css/App.css';
+import {
+  BrowserRouter,
+  Route
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="container">
-     
-    </div>
-  );
-}
+
+import Home from "./Home"
+import About from "./About"
+// import Header from "./Header"
+
+const App = () => (
+      <BrowserRouter>
+        <div className="container">
+          
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+        </div>
+      </BrowserRouter>
+)
 
 export default App;
