c0ba5b888125544df0b691263db5471c4c832bbe
DelfinMong <delfdevs@gmail.com>
2021-11-27 23:21:09 -0500

    ballprops

--- lottery/src/App.js
+++ lottery/src/App.js
@@ -1,11 +1,18 @@
+import React,{Component} from 'react';
 import './App.css';
+import Ball from './Ball';
 
-function App() {
-  return (
-    <div className="App">
-     <p> LOTTERY </p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <Ball num = {17}/> 
+         <Ball num = {13}/> 
+         <Ball num = {11}/> 
+         <Ball num = {19}/> 
+      </div>
+    );
+  }
 }
 
 export default App;
