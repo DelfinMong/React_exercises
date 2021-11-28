3e0c192439b81705ca693859bd3fd2e07ac11b99
DelfinMong <delfdevs@gmail.com>
2021-11-27 23:26:47 -0500

    defaultprops

---
+++ lottery/src/Lotter.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+
+class Lottery extends Component {
+    static defaultProps = {
+        title: "Lotto",
+        maxBalls: 6,
+        maxNum: 40
+    }
+    constructor(){
+        super(props);
+        this.state = {
+            nums: []
+        }
+    }
+    render() {
+        return (
+           <section className="Lottery">
+            <h1>{this.props.title}</h1>
+            <div>
+
+            </div>
+           </section> 
+        )
+    }
+}
+
+export default Lottery;
