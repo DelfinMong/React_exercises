60c599cde034bcc84ad98b3dbd392c97f769d54b
DelfinMong <delfdevs@gmail.com>
2021-11-29 00:38:01 -0500

    emptyball

--- lottery/src/App.js
+++ lottery/src/App.js
@@ -1,15 +1,12 @@
 import React,{Component} from 'react';
 import './App.css';
-import Ball from './Ball';
+import Lottery from './Lottery';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <Ball num = {17}/> 
-         <Ball num = {13}/> 
-         <Ball num = {11}/> 
-         <Ball num = {19}/> 
+       <Lottery/>
       </div>
     );
   }
