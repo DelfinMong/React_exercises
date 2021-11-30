6a7482b30be60eb18868671992ac81d9e1f9de49
DelfinMong <delfdevs@gmail.com>
2021-11-30 02:22:06 -0500

    newstate

--- coin-flipper/src/CoinContainer.js
+++ coin-flipper/src/CoinContainer.js
@@ -1,10 +1,11 @@
 import React,{Component} from 'react'
+import { choice } from './helpers'
 
 class CoinContainer extends Component {
     static defaultProps = {
-        couns: [
+        coins: [
             {sides: 'heads', url: "https://tinyurl.com/react-coin-heads-jpg"},
-            {side: 'tails', url: "https://tinyurl.com/react-coin-tails-jpg"}
+            {sides: 'tails', url: "https://tinyurl.com/react-coin-tails-jpg"}
         ]
     }
     constructor(){
@@ -17,16 +18,28 @@ class CoinContainer extends Component {
         };
         this.handleClick = this.handleClick.bind(this);
     }
-    flicCoin(){
-
+    flipCoin(){
+        const newCoin = choice(this.props.coins);
+        this.setState( st => {
+          let newState = {
+            ...st,
+            currCoin: newCoin,
+            nFlips: st.nFlips + 1
+            // return {
+            //    currCoin: newCoin,
+            //    nFlips: st.nFlips + 1
+            // }
+          }
+        })
     }
     handleClick(e){
-
+        this.flipCoin();
     }
     render(){
         return(
         <div className="CoinContainer">
             <h2>Let's Flip A Coin</h2>
+            <button onClick={this.handleClick}> Flip Me!</button>
             <p>
                Out of {this.state.nFlips} Flips, there have been {this.state.nHeads} {" "}
                heads and {this.state.nTails} tails.
