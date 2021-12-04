7ab021f942e3adfc5ce0ae0214be63746f9dedbf
DelfinMong <delfdevs@gmail.com>
2021-12-03 22:18:58 -0500

    helperfunc

--- color-boxes/src/App.js
+++ color-boxes/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Box from './Box'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      color boxes
+      <Box/>
     </div>
   );
 }
---
+++ color-boxes/src/Box.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+import { choice } from './Helpers';
+import './Box.css'
+
+class Box extends Component {
+    static defaultProps = {
+        allColors: ['purple','magenta','violet','pink']
+    };
+    constructor(props){
+        super(props);
+        this.state = { color: choice(this.props.allColors) };
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(){
+
+    }
+    render(){
+        return(
+            <div className="Box" 
+                 style={{ backgroundColor: this.state.color}} 
+                 onClick={this.handleClick}
+            />
+
+        ) 
+    }
+}
+
+export default Box;
