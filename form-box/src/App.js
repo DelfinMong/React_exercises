d2c5ceb6a935cee0c2e836e1ca87341c0566fce5
DelfinMong <delfdevs@gmail.com>
2021-12-06 00:01:54 -0500

    boxform

--- form-box/src/App.js
+++ form-box/src/App.js
@@ -1,12 +1,17 @@
-import Box from './Box'
+import BoxList from './BoxList';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+  render(){
   return (
-    <div className="App">
-      <Box/>
+    <div>
+      {/* <Box height={10} width={20} color="purple" />
+      <Box height={20} width={15} color="teal" /> */}
+      <BoxList />
     </div>
   );
+ }
 }
 
 export default App;
--- form-box/src/Box.js
+++ form-box/src/Box.js
@@ -1,13 +1,13 @@
-import React, { Component, component } from 'react'
+import React, { Component } from 'react'
 
 class Box extends Component {
     render() {
         return (
             <div>
                 <div style={{
-                        height:`${this.props.height}em`,
-                        width:`${this.props.width}em`,
-                        backgroundColor: this.props.color
+                       height:`${this.props.height}em`,
+                       width:`${this.props.width}em`,
+                       backgroundColor: this.props.color
                      }}
                 />
                 <button>X</button>
