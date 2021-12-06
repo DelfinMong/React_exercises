29af0b9be3af8d3d58346b9ac986975080e559c6
DelfinMong <delfdevs@gmail.com>
2021-12-06 02:08:26 -0500

    fillform

--- form-box/src/NewBoxForm.js
+++ form-box/src/NewBoxForm.js
@@ -1,12 +1,26 @@
 import Reat,{ Component } from 'react';
 
 class NewBoxForm extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            height: '',
+            width: '',
+            colot:''
+        }
+    }
     render(){
         return(
             <form>
                 <div>
                     <label>Height</label>
-                    <input type='text' />
+                    <input 
+                      type='text'
+                      name='height'
+                      value={this.state.height}
+                      onChange={this.handleChange}
+                      id='height' 
+                    />
                 </div>
             </form>
         )
