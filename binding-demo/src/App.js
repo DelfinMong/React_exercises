797ec70c562a2817c154b402acfcebf708c3c2dd
DelfinMong <delfdevs@gmail.com>
2021-12-07 01:42:12 -0500

    betterNumber

--- binding-demo/src/App.js
+++ binding-demo/src/App.js
@@ -3,11 +3,15 @@ import WiseSquare from './WiseSquareWithProps'
 import AnnoyingForm from './AnnoyingForm'
 import CopyDemo from './CopyDemo';
 import ButtonLit from './ButtonList';
+import NumberList from './NumberList';
+import BetterNumberList from './BetterNumberList'
 
 function App() {
   return (
     <div className="App">
       Biding Demo
+      <NumberList />
+      <BetterNumberList />
       <ButtonLit />
       <WiseSquare />
       <AnnoyingForm />
