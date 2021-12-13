7ac206a529450e0c177da3cb407082f26ef0641b
DelfinMong <delfdevs@gmail.com>
2021-12-13 00:36:23 -0500

    uuid

--- 12.form/src/Shopping/ShoppingList.js
+++ 12.form/src/Shopping/ShoppingList.js
@@ -1,13 +1,14 @@
 import React,{Component} from 'react'
 import ShoppingListForm from './ShoppingListForm'
+import {v1 as uuid} from 'uuid';
 
 class ShoppingList extends Component {
    constructor(props){
        super(props)
        this.state = {
            items: [
-               {name: 'Milk', qty: '2 gallons'},
-               {name: 'Bread', qty: '2 loaves'}
+               {name: 'Milk', qty: '2 gallons', id: uuid()},
+               {name: 'Bread', qty: '2 loaves', id: uuid()}
            ]
        }
        this.addItem = this.addItem.bind(this)
@@ -22,7 +23,7 @@ class ShoppingList extends Component {
             <div>
               <ul>
                   {this.state.items.map( item => (
-                      <li>
+                      <li key={item.id}> 
                           {item.name}: {item.qty}
                       </li>
                   ))}
