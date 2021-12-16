df9436bdb9b5ad3ef7bb01bb459d8750024ca0d5
DelfinMong <delfdevs@gmail.com>
2021-12-15 23:46:17 -0500

    todolist

---
+++ todo-list/src/TodoList.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+
+class TodoList extends Component {
+    render(){
+       return (
+        <div className="App">
+            Slim 600
+        </div>
+       );
+    }
+}
+
+export default TodoList;
