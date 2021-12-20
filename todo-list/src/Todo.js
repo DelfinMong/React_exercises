837bc7b655039cf258fa899e5b41c7ea7546a670
DelfinMong <delfdevs@gmail.com>
2021-12-20 14:20:45 -0500

    handleUpdate

--- todo-list/src/Todo.js
+++ todo-list/src/Todo.js
@@ -5,28 +5,35 @@ import TodoList from './TodoList';
 class Todo extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isEditing: false
-        }
+        this.state = { isEditing: false }
         this.handleRemove = this.handleRemove.bind(this)
+        this.toggleForm = this.toggleForm.bind(this)
+        this.handleUpdatev = this.handleUpdate(this)
     }
     handleRemove(){
         this.props.removeTodo(this.props.id)
+    }
+    toggleForm(){
+        this.setState({ isEditing: !this.state.isEditing})
+    }
+    handleUpdate(evt){
+        evt.preventDefault()
     }
     render(){
        let result;
        if(this.state.isEditing){
             result = (
                 <div>
-                  <form>
+                  <form onSubmit={this.handleUpdate}>
                       <input type="text" />
+                      <button>Save</button>
                   </form>
                 </div>
             )
        } else {
            result = (
             <div>
-                <button>Edit</button>
+                <button onClick={this.toggleForm}>Edit</button>
                 <button onClick={this.handleRemove}>X</button>
                 <li>{this.props.task}</li>
             </div>
