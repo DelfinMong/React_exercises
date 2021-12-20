9c8a00b265a4940295640aaf13b7378eb10c77fa
DelfinMong <delfdevs@gmail.com>
2021-12-20 13:58:35 -0500

    isEditing

--- todo-list/src/Todo.js
+++ todo-list/src/Todo.js
@@ -3,14 +3,36 @@ import TodoList from './TodoList';
 
 
 class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false
+        }
+        this.handleRemove = this.handleRemove.bind(this)
+    }
+    handleRemove(){
+        this.props.removeTodo(this.props.id)
+    }
     render(){
-       return (
-        <div>
-          <button>Edit</button>
-          <button>X</button>
-          <li>{this.props.task}</li>
-        </div>
-       );
+       let result;
+       if(this.state.isEditing){
+            result = (
+                <div>
+                  <form>
+                      <input type="text" />
+                  </form>
+                </div>
+            )
+       } else {
+           result = (
+            <div>
+                <button>Edit</button>
+                <button onClick={this.handleRemove}>X</button>
+                <li>{this.props.task}</li>
+            </div>
+           )
+       }
+       return result;
     }
 }
 
