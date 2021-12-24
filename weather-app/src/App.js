d02f1e9b654755e040e0fade5928a331e49590a6
DelfinMong <delfdevs@gmail.com>
2021-12-23 23:55:48 -0500

    weatherapp

---
+++ weather-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+class Title extends React.Component{
+  
+  render(){
+  return (
+    <div>
+     slimm 400
+
+    </div>
+   )
+  };
+}
+
+export default Title;
---
+++ weather-app/src/Titles.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+class App extends React.Component{
+  
+  render(){
+  return (
+    <div>
+     slimm 400
+
+    </div>
+   )
+  };
+}
+
+export default App;
