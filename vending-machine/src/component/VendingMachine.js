57f5cf10188acfbb6509997a8ecbe5237f91b3b7
DelfinMong <delfdevs@gmail.com>
2021-12-29 03:01:00 -0500

    css

---
+++ vending-machine/src/component/VendingMachine.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import '../css/VendingMachine.css'
+
+class VendingMachine extends Component{
+    render(){
+    return (
+      <div>
+      
+      </div>
+    );
+  }
+}
+
+export default VendingMachine;
