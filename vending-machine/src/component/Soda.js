a703aedadb6c2987e24dee070c4579111f173810
DelfinMong <delfdevs@gmail.com>
2021-12-29 11:44:50 -0500

    soda

--- vending-machine/src/component/Soda.js
+++ vending-machine/src/component/Soda.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
 // import Message from "./Message";
-// import { Link } from "react-router-dom";
-// import sodaImg from "./Soda.png";
-// import "./Soda.css";
+import { Link } from "react-router-dom";
+import sodaImg from "../img/Soda.png";
+import "../css/Soda.css";
 
 class Soda extends Component {
   render() {
     return (
       <div className='Soda'>
-        {/* <img src={sodaImg} alt='coca cola can' /> */}
+        <img src={sodaImg} alt='coca cola can' />
         {/* <Message> */}
           <h1>SODAAAAA IS MY FAVORITE</h1>
           <h3>But now I drink sparkling water instead</h3>
           <p>(I still miss Soda)</p>
-          {/* <Link to='/'>Go Back</Link> */}
+          <Link to='/' style={{color:'red'}}>Go Back</Link>
         {/* </Message> */}
 
-        {/* <img src={sodaImg} alt='coca cola can' /> */}
+        <img src={sodaImg} alt='coca cola can' />
       </div>
     );
   }
--- vending-machine/src/component/VendingMachine.js
+++ vending-machine/src/component/VendingMachine.js
@@ -1,11 +1,19 @@
 import { Component } from 'react';
-import '../css/VendingMachine.css'
+import { Link } from 'react-router-dom';
+import '../css/VendingMachine.css';
+import vendingMachineImg from '../img/VendingMachine.png';
 
 class VendingMachine extends Component{
     render(){
     return (
-      <div>
-      
+      <div
+      className={ ({ isActive }) => isActive ? "VendingMachine":  "  " }
+      style={{ backgroundImage: `url(${vendingMachineImg})` }}
+      >
+      <h1>hello i am vending maching. what would you like to eat </h1>
+      <Link to='/soda'>Soda</Link>
+      <Link to='/chips'>Chips</Link>
+      <Link to='/sardines'>Sardines</Link>
       </div>
     );
   }
